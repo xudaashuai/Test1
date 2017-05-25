692d6ac0631120cd07fb02fb33b3a920f7bc5439
xudaashuai <xudashuai0819@outlook.com>
2017-05-25 08:18:54 +0800

    1

--- tt.py
+++ tt.py
@@ -71,25 +71,25 @@ class MTopo(Topo):
 
 class MMTopo(Topo):
     def build(self):
-        s1 = self.addSwitch('s1', ip='127.1.0.0/16')
-        s2 = self.addSwitch('s2', ip='127.2.0.0/16')
-        s3 = self.addSwitch('s3', ip='127.1.0.1')
-        s4 = self.addSwitch('s4', ip='127.1.0.2')
-        s5 = self.addSwitch('s5', ip='127.2.0.1')
-        s6 = self.addSwitch('s6', ip='127.2.0.2')
-        s7 = self.addSwitch('s7', ip='127.1.1.0/24')
-        s8 = self.addSwitch('s8', ip='127.1.2.0/24')
-        s9 = self.addSwitch('s9', ip='127.2.1.0/24')
-        s10 = self.addSwitch('s10', ip='127.2.2.0/24')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
+        s6 = self.addSwitch('s6')
+        s7 = self.addSwitch('s7')
+        s8 = self.addSwitch('s8')
+        s9 = self.addSwitch('s9')
+        s10 = self.addSwitch('s10')
 
-        h1 = self.addHost('h1', ip='10.0.0.1')
-        h2 = self.addHost('h2', ip='10.0.0.2')
-        h3 = self.addHost('h3', ip='20.0.0.1')
-        h4 = self.addHost('h4', ip='20.0.0.2')
-        h5 = self.addHost('h5', ip='30.0.0.1')
-        h6 = self.addHost('h6', ip='30.0.0.2')
-        h7 = self.addHost('h7', ip='40.0.0.1')
-        h8 = self.addHost('h8', ip='40.0.0.2')
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        h7 = self.addHost('h7')
+        h8 = self.addHost('h8')
         # Add links
         self.addLink(s1, s3)
         self.addLink(s1, s4)
@@ -118,10 +118,7 @@ class MMTopo(Topo):
 topos = { 'mytopo': ( lambda: MMTopo() ) }
 
 def demo():
-    net = Mininet(topo=MMTopo(), cleanup=True, link=TCLink,
-                  autoSetMacs=True, autoStaticArp=True,
-                  switch=OVSSwitch
-                  )
+    net = Mininet(topo=MMTopo())
     net.start()
     #cli = CLI(net,script='b.txt')
     CLI(net)
